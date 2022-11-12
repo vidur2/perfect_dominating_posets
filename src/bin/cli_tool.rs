7a58a4a6b84f5f9039b750adc385fc98d82ce847
vidur2 <vmod2005@gmail.com>
2022-11-12 13:42:05 -0500

    better?

---
+++ src/bin/cli_tool.rs
@@ -0,0 +1,31 @@
+#![feature(let_chains)]
+
+use std::{fs::File, io::Read};
+
+use perfect_dominating_posets::interval::Interval;
+use perfect_dominating_posets::preprocessing::parse_buff;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() == 2 {
+        if let Ok(mut content) = File::open(&args[1]) {
+            // Read File into adjacency matrix
+            let mut buf = [0u8; 1024];
+            let size = content.read(&mut buf).unwrap();
+            let file_str = String::from_utf8(buf[0..size].to_vec()).unwrap();
+            let (upsets_downsets, topology_list) = parse_buff(file_str);
+            let interval = Interval::new(upsets_downsets);
+
+            if let Ok(interval) = interval && let Ok(pds) = interval.find_dominating_set(topology_list){
+                println!("{:?}", pds);
+            } else {
+                println!("Error generating dominating set, poset outside of scope of algorithm")
+            }
+        } else {
+            println!("Error: Please enter a valid path")
+        }
+    } else {
+        println!("Error: Please enter a valid number of arguments")
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+#![feature(string_remove_matches)]
+#![feature(let_chains)]
+
+pub mod interval;
+pub mod preprocessing;
