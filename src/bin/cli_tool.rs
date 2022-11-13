42b16819cdc9cb6515db2bdff0827595c1e4a4f8
vidur2 <vmod2005@gmail.com>
2022-11-13 14:31:24 -0500

    made implementation generic

--- src/bin/cli_tool.rs
+++ src/bin/cli_tool.rs
@@ -1,9 +1,45 @@
 #![feature(let_chains)]
+#![feature(string_remove_matches)]
 
 use std::{fs::File, io::Read};
 
 use perfect_dominating_posets::interval::Interval;
-use perfect_dominating_posets::preprocessing::parse_buff;
+use perfect_dominating_posets::preprocessing::{parse_buff, GraphConvertable, EdgeLike};
+
+struct FileStr(String);
+
+struct EdgeString(String);
+
+impl GraphConvertable<EdgeString> for FileStr {
+    fn split(&self) -> Vec<EdgeString> {
+        let string = self.0.clone();
+        let mut edge_vec: Vec<EdgeString> = Vec::new();
+        for part in string.split("\n") {
+            edge_vec.push(EdgeString(String::from(part)))
+        }
+        
+        return edge_vec;
+    }
+}
+
+impl EdgeLike for EdgeString {
+    fn split_edge(&self) -> (u8, u8) {
+        let mut edge = self.0.clone();
+        edge.remove_matches("[");
+        edge.remove_matches("]");
+
+        let nums: Vec<&str> = edge.split(",").collect();
+
+        let mut out: [u8; 2] = [0; 2];
+
+        for (idx, num) in nums.iter().enumerate() {
+            let num: u8 = num.to_string().parse().unwrap();
+            out[idx] = num;
+        }
+
+        return (out[0], out[1]);
+    }
+}
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -14,7 +50,7 @@ fn main() {
             let mut buf = [0u8; 1024];
             let size = content.read(&mut buf).unwrap();
             let file_str = String::from_utf8(buf[0..size].to_vec()).unwrap();
-            let (upsets_downsets, topology_list) = parse_buff(file_str);
+            let (upsets_downsets, topology_list) = parse_buff::<EdgeString, _>(FileStr(file_str));
             let interval = Interval::new(upsets_downsets);
 
             if let Ok(interval) = interval && let Ok(pds) = interval.find_dominating_set(topology_list){
--- src/preprocessing.rs
+++ src/preprocessing.rs
@@ -23,67 +23,37 @@ pub enum VecInner {
 #[derive(Debug, Clone)]
 pub struct Set(pub Rc<RefCell<Vec<VecInner>>>);
 
+pub trait GraphConvertable<T: EdgeLike> {
+    fn split(&self) -> Vec<T>;
+}
+
+pub trait EdgeLike {
+    fn split_edge(&self) -> (u8, u8);
+}
+
 // Parsing method, reads file into Upsets and Downsets
-pub fn parse_buff(buff: String) -> (HashMap<u8, UpDown>, HashMap<u8, HashSet<u8>>) {
+pub fn parse_buff<I: EdgeLike, T: GraphConvertable<I>>(buff: T) -> (HashMap<u8, UpDown>, HashMap<u8, HashSet<u8>>) {
 
     // Declaring of upset/downset and degree list
     let mut map: HashMap<u8, UpDown> = HashMap::new();
     let mut topology_list: HashMap<u8, HashSet<u8>> = HashMap::new();
 
     // Iterating through file
-    for edge in buff.split("\n") {
+    for edge in buff.split() {
 
         // Splitting file into meaningful edges
-        let (v1, v2) = parse_edge(edge);
+        let (v1, v2) = edge.split_edge();
 
         // Adding to adjacency list
-        if let Some(deg) = topology_list.get_mut(&v1) {
-            deg.insert(v2);
-        } else {
-            let mut deg = HashSet::new();
-            deg.insert(v2);
-            topology_list.insert(v1, deg);
-        }
+        add_to_toplist(&mut topology_list, v1, v2);
 
         // Same as above
-        if let Some(deg) = topology_list.get_mut(&v2) {
-            deg.insert(v1);
-        } else {
-            let mut deg = HashSet::new();
-            deg.insert(v1);
-            topology_list.insert(v2, deg);
-        }
+        add_to_toplist(&mut topology_list, v2, v1);
 
         // Adding to upset/downsets
-        if let Some(adj_list) = map.get_mut(&v1) {
-            adj_list.upset.0.borrow_mut().push(VecInner::Num(v2));
-        } else {
-            map.insert(
-                v1,
-                UpDown {
-                    downset: Set(Rc::new(RefCell::new(Vec::new()))),
-                    upset: Set(Rc::new(RefCell::new(vec![VecInner::Num(v2)]))),
-                },
-            );
-        }
+        add_to_updown(&mut map, v1, v2);
 
-        let new_item2 = Rc::clone(&map.get(&v1).unwrap().downset.0);
-        if let Some(adj_list) = map.get_mut(&v2) {
-            let mut borrow = adj_list.downset.0.borrow_mut();
-            borrow.push(VecInner::Num(v1));
-            borrow.push(VecInner::Vec(Rc::downgrade(&new_item2)));
-        } else {
-            map.insert(
-                v2,
-                UpDown {
-                    downset: Set(Rc::new(RefCell::new(vec![
-                        VecInner::Num(v1),
-                        VecInner::Vec(Rc::downgrade(&new_item2)),
-                    ]))),
-                    upset: Set(Rc::new(RefCell::new(Vec::new()))),
-                },
-            );
-        }
+        add_to_updown2nd(&mut map, v1, v2);
 
         let new_item = Rc::clone(&map.get(&v2).unwrap().upset.0);
         let mut downlist = map.get_mut(&v1).unwrap().upset.0.borrow_mut();
@@ -92,6 +62,50 @@ pub fn parse_buff(buff: String) -> (HashMap<u8, UpDown>, HashMap<u8, HashSet<u8>
     }
 
     return (map, topology_list);
+}
+
+fn add_to_updown2nd(map: &mut HashMap<u8, UpDown>, v1: u8, v2: u8) {
+    let new_item2 = Rc::clone(&map.get(&v1).unwrap().downset.0);
+    if let Some(adj_list) = map.get_mut(&v2) {
+        let mut borrow = adj_list.downset.0.borrow_mut();
+        borrow.push(VecInner::Num(v1));
+        borrow.push(VecInner::Vec(Rc::downgrade(&new_item2)));
+    } else {
+        map.insert(
+            v2,
+            UpDown {
+                downset: Set(Rc::new(RefCell::new(vec![
+                    VecInner::Num(v1),
+                    VecInner::Vec(Rc::downgrade(&new_item2)),
+                ]))),
+                upset: Set(Rc::new(RefCell::new(Vec::new()))),
+            },
+        );
+    }
+}
+
+fn add_to_updown(map: &mut HashMap<u8, UpDown>, v1: u8, v2: u8) {
+    if let Some(adj_list) = map.get_mut(&v1) {
+        adj_list.upset.0.borrow_mut().push(VecInner::Num(v2));
+    } else {
+        map.insert(
+            v1,
+            UpDown {
+                downset: Set(Rc::new(RefCell::new(Vec::new()))),
+                upset: Set(Rc::new(RefCell::new(vec![VecInner::Num(v2)]))),
+            },
+        );
+    }
+}
+
+fn add_to_toplist(topology_list: &mut HashMap<u8, HashSet<u8>>, v1: u8, v2: u8) {
+    if let Some(deg) = topology_list.get_mut(&v1) {
+        deg.insert(v2);
+    } else {
+        let mut deg = HashSet::new();
+        deg.insert(v2);
+        topology_list.insert(v1, deg);
+    }
 }
 
 fn parse_edge(edge: &str) -> (u8, u8) {
